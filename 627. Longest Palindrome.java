63fd62c208924c219921e7e682684bde032c93f2
Tian Feng <feng.txy@outlook.com>
2020-04-16 22:50:05 -0700

    Update 627. Longest Palindrome.java

--- 627. Longest Palindrome.java
+++ 627. Longest Palindrome.java
@@ -23,3 +23,49 @@ public class Solution {
         return s.length() - remove;
     }
 }
+
+
+
+public class Solution {
+    /**
+     * @param s: a string which consists of lowercase or uppercase letters
+     * @return: the length of the longest palindromes that can be built
+     */
+    public int longestPalindrome(String s) {
+        // write your code here
+        int[] charStatArray = new int[52];
+        int oneTimeOddCount = 0;
+        int evenCount = 0;
+        
+        for(char ch : s.toCharArray()){
+            if (ch >= 97){
+                charStatArray[26 + ch - 'a'] ++;
+            }
+            else{
+                charStatArray[ch - 'A'] ++;
+            }
+        }
+        
+        for(int cnt:charStatArray){
+            if(cnt != 0){
+                if(cnt % 2 == 0){
+                    evenCount += cnt;
+                }
+                else{
+                    if(cnt == 1){
+                        oneTimeOddCount++;
+                    }
+                    else{
+                        evenCount += cnt - 1;
+                        oneTimeOddCount++;
+                    }
+                }
+                
+            }
+        }
+        
+        return oneTimeOddCount>0? 1+evenCount:evenCount;
+        
+        
+    }
+}
