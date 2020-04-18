2f1a44e9f82255f79f57815a2a63357ecb32f068
Tian Feng <feng.txy@outlook.com>
2020-04-18 12:00:45 -0700

    Update 433. Number of Islands.java

--- 433. Number of Islands.java
+++ 433. Number of Islands.java
@@ -35,6 +35,33 @@ public class Solution {
     private void markByBFS(boolean[][] grid, int x, int y){
         
     }
+        private void markByBFS(boolean[][] grid, int x, int y) {
+        // magic numbers!
+        int[] directionX = {0, 1, -1, 0};
+        int[] directionY = {1, 0, 0, -1};
+        
+        Queue<Coordinate> queue = new LinkedList<>();
+        
+        queue.offer(new Coordinate(x, y));
+        grid[x][y] = false;
+        
+        while (!queue.isEmpty()) {
+            Coordinate coor = queue.poll();
+            for (int i = 0; i < 4; i++) {
+                Coordinate adj = new Coordinate(
+                    coor.x + directionX[i],
+                    coor.y + directionY[i]
+                );
+                if (!inBound(adj, grid)) {
+                    continue;
+                }
+                if (grid[adj.x][adj.y]) {
+                    grid[adj.x][adj.y] = false;
+                    queue.offer(adj);
+                }
+            }
+        }
+    }
     
     
 }
